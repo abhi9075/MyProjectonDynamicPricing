1b7bf8c9c59ab625f017304c5d6e309ae6ecb87d
Abhishek Kumar <abhi9075@gmail.com>
2023-11-16 20:50:23 +0530

    changes

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/')
+def welcome():
+    return " Welcome to project file"
+
 if __name__ =='__main__':
-    app.run()
+    app.run(debug=True)
